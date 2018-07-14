3fe77776354069346f92671de26b0654765f49ae
Famulski.Michal <187684@edu.p.lodz.pl>
2018-07-14 07:04:16 +0200

    isolation Added test cases when issuing invoice with zero positions

--- src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeperTest.java
@@ -1,26 +1,31 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
-import org.junit.Test;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
 
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
-
 public class BookKeeperTest {
+
     InvoiceFactory invoiceFactory;
     InvoiceRequest invoiceRequest;
     TaxPolicy taxPolicy;
     BookKeeper bookKeeper;
     ProductData productData;
-    
+
     @Before
     public void setUp() throws Exception {
         taxPolicy = mock(TaxPolicy.class);
@@ -29,8 +34,10 @@ public class BookKeeperTest {
         ClientData client = mock(ClientData.class);
         invoiceRequest = new InvoiceRequest(client);
         productData = mock(ProductData.class);
-        
-        invoiceFactory = new InvoiceFactory();
+
+        invoiceFactory = mock(InvoiceFactory.class);
+        when(invoiceFactory.create(any(ClientData.class)))
+                .thenReturn(new Invoice(Id.generate(), new ClientData(Id.generate(), "")));
         bookKeeper = new BookKeeper(invoiceFactory);
     }
 
@@ -43,16 +50,29 @@ public class BookKeeperTest {
         int positions = result.getItems().size();
         assertThat(positions, Matchers.equalTo(1));
     }
+
     @Test
     public void issuingInvoiceWithTwoPositionsShouldCallTaxPolicyMethodTwoTimesWithParametersMatchingInvoicePositions() {
         RequestItem item = new RequestItem(productData, 4, new Money(5));
         RequestItem secondItem = new RequestItem(productData, 2, new Money(4));
         invoiceRequest.add(item);
         invoiceRequest.add(secondItem);
-        Invoice result = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
         verify(taxPolicy, atMost(2)).calculateTax(any(ProductType.class), any(Money.class));
         verify(taxPolicy, times(1)).calculateTax(item.getProductData().getType(), item.getTotalCost());
         verify(taxPolicy, times(1)).calculateTax(secondItem.getProductData().getType(), secondItem.getTotalCost());
-        }
+    }
 
+    @Test
+    public void issuingInvoiceWithZeroPositionsShouldReturnInvoiceWithZeroPositions() {
+        Invoice result = bookKeeper.issuance(invoiceRequest, taxPolicy);
+        int positions = result.getItems().size();
+        assertThat(positions, Matchers.equalTo(0));
+    }
+
+    @Test
+    public void issuingInvoiceWithZeroPositionsShouldCallTaxPolicyMethodZeroTimes() {
+        bookKeeper.issuance(invoiceRequest, taxPolicy);
+        verify(taxPolicy, atMost(0)).calculateTax(any(ProductType.class), any(Money.class));
+    }
 }
